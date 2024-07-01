20821b149184ce40d5c1a2409ee87a6c33c0e1ec
Kaka <vn503024@gmail.com>
2024-07-02 01:40:36 +0800

    chore: update docker file and running script

--- docker-run.sh
+++ docker-run.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
-IMAGE_NAME="kakalin/qt:5.12.0_opencv_gstreamer"
+XSOCK=/tmp/.X11-unix
+# change the XAUTH to your own .Xauthority file
+XAUTH=/root/.Xauthority
+
+DOCKER_REPO=kakalin/
+BRAND=qt
+VERSION=5.12.0_opencv_gstreamer
+IMAGE_NAME=${DOCKER_REPO}${BRAND}:${VERSION}
+
+# https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
+PROJECT_FOLDER=${PWD##*/}
+
+GPU_DEVICE=""
+if [[ -z $1 ]]; then
+    GPU_DEVICE="all"
+else
+    GPU_DEVICE="device=$1"
+fi
+
+# -e QT_X11_NO_MITSHM=1: Disables MIT-SHM extension for Qt applications.
+# -e XAUTHORITY=$XAUTH: Sets the XAUTHORITY environment variable to the .Xauthority file.
+# -v $XSOCK:$XSOCK: Mounts the X11 socket to the container.
+# -v $HOME/.Xauthority:$XAUTH: Mounts the .Xauthority file to the container.
+# --network=host: Shares the host network with the container.
+# --privileged: Enables privileged mode for the container.s
 docker_run_params=$(cat <<-END
     --rm -it \
     -e QT_X11_NO_MITSHM=1 \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $PWD:/home/user/YUVPlayer \
+    -e XAUTHORITY=$XAUTH \
+    -v $XSOCK:$XSOCK \
+    -v $HOME/.Xauthority:$XAUTH \
     --network=host \
-    -v $HOME/.Xauthority:/root/.Xauthority \
-    -w /home/user/YUVPlayer \
+    -v $PWD:/root/$PROJECT_FOLDER \
+    -w /root/$PROJECT_FOLDER \
     --privileged \
     $IMAGE_NAME
 END
 )
 
-xhost +localhost
-xhost +local:docker
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     GPU=$(lspci | grep -i '.* vga .* nvidia .*')
     if [[ $GPU == *' NVIDIA '* ]]; then
-        # printf 'Nvidia GPU is present:  %s\n' "$GPU"
+        # -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native: Sets the PulseAudio server environment variable.
+        # -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native: Mounts the PulseAudio socket to the container.
+        # -v $PWD:/home/user/$PROJECT_FOLDER: Mounts the current directory to the container.
         docker run \
-            --gpus all \
+            --gpus $GPU_DEVICE \
             -e DISPLAY=$DISPLAY \
             -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
             -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
             $docker_run_params
     else
         docker run \
@@ -32,6 +58,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             -e DISPLAY=$DISPLAY \
             -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
             -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
             $docker_run_params
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
